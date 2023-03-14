fe2243ffc0ea332eff0ce43b5f0e89e1959be398
jabir11757 <smjalsaba11757@gmail.com>
2023-03-14 13:42:17 +0600

    API created

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -16,5 +16,20 @@ const getUser = async (req, res) => {
     res.send(user)
 }
 
+//delete user for task-5
+const deleteUser = async (req, res) => {
+    const user = await Users.deleteOne({ _id: req.params.id })
+    res.send(user)
+}
+
+
+//delete user for task-4 {I used client as user to avoid match task-4 & task-5 matches name of user}
+const deleteClient = async (req, res) => {
+    const client = await Users.deleteOne({ _id: req.params.id })
+    res.send(client)
+}
+
 exports.postUser = postUser;
 exports.getUser = getUser;
+exports.deleteUser = deleteUser;
+exports.deleteClient = deleteClient;
